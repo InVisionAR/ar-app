25aeccc559059e56653dcc472845619405bd7412
shricharanks108 <62522322+shricharanks108@users.noreply.github.com>
2020-11-04 10:59:41 -0500

    Add Basic AR

---
+++ Assets/Scripts/PlacementIndicatorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicatorScript : MonoBehaviour
+{
+    private ARRaycastManager raycastManager;
+    private GameObject indicator;
+
+    void Start()
+    {
+        // get the components
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+        indicator = transform.GetChild(0).gameObject;
+
+        // hide the placement indicator visual
+        indicator.SetActive(false);
+    }
+
+    void Update()
+    {
+        // shoot a raycast from the center of the screen
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        if (hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            // enable the visual if it's disabled
+            if (!indicator.activeInHierarchy)
+                indicator.SetActive(true);
+        }
+    }
+}
