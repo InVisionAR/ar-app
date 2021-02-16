ebf24ecedbc7d6dcfaf55f78b51934551d805930
Sagar Patil <sagarreddypatil@gmail.com>
2021-02-16 17:24:43 -0500

    Finish procedural scene building. TODO: Make simulation work with Pause/Play and Physic Material Editing

--- Assets/Scripts/Procedural Simulations/GameManager.cs
+++ Assets/Scripts/Procedural Simulations/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public enum SimulationObjectType
 {
-    RectangularPrism,
+    RectangularPrism=1,
     Sphere,
     TriangularPrism,
     RemoteMesh,
@@ -21,13 +21,22 @@ public class SimulationObjectMaterial
     public float Smoothness;
 }
 
+[Serializable]
+public class SimulationObjectPhysicMaterial
+{
+    public float bounciness;
+    public float dynamicFriction;
+    public float staticFriction;
+    public PhysicMaterialCombine frictionCombine;
+    public PhysicMaterialCombine bounceCombine;
+}
 
 [Serializable]
 public class SimulationObject
 {
     public SimulationObjectType Type;
     public SimulationObjectMaterial Material;
-    public PhysicMaterial PhysicMaterial;
+    public SimulationObjectPhysicMaterial PhysicMaterial;
     public Vector3 Position;
     public Vector3 EulerRotation;
     public Vector3 Scale;
@@ -44,6 +53,12 @@ public class SimulationConfig
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject simulationInitializatonPoint;
+    
+    [Header("Primative Prefabs")]
+    [SerializeField]
+    private GameObject rectangularPrismPrefab;
+
     void Start()
     {
         var config = new SimulationConfig
@@ -55,17 +70,46 @@ public class GameManager : MonoBehaviour
                 new SimulationObject
                 {
                     Type = SimulationObjectType.RectangularPrism,
-                    Material = null,
-                    PhysicMaterial = new PhysicMaterial { staticFriction = 1.0f, dynamicFriction = 1.0f, bounciness = 0.3f },
+                    Material = new SimulationObjectMaterial {Color=new Color(1.0f, 0.0f, 0.0f), Metallicness=1.0f, Smoothness=1.0f},
+                    PhysicMaterial = new SimulationObjectPhysicMaterial { staticFriction = 0.50f, dynamicFriction = 0.7f, bounciness = 0.3f },
                     Position = new Vector3(0, 0, 0.5f),
                     EulerRotation = new Vector3(0, 0, 0),
-                    Scale = new Vector3(1f, 1f, 1f)
+                    Scale = new Vector3(0.5f, 5f, 3f)
                 }
             }
         };
 
-        string configJson = JsonUtility.ToJson(config);
-        Debug.Log(configJson);
+        foreach(SimulationObject simulationObject in config.Objects){
+            BuildSimulationObject(simulationObject);
+        }
+
+        // string configJson = JsonUtility.ToJson(config);
+        // Debug.Log(configJson); <-- works as expected
+    }
+
+    void BuildSimulationObject(SimulationObject simulationObject){
+        GameObject gameObject = null;
+        switch(simulationObject.Type){
+            case SimulationObjectType.RectangularPrism:
+                gameObject = (GameObject)Instantiate(rectangularPrismPrefab, simulationObject.Position, Quaternion.Euler(simulationObject.EulerRotation), simulationInitializatonPoint.transform);
+                break;
+            case 0:
+                break;
+        }
+        gameObject.transform.localScale = simulationObject.Scale;
+
+        Material gameObjectMat = gameObject.GetComponent<Renderer>().material;
+        PhysicMaterial gameObjectPhysicMat = gameObject.GetComponent<Collider>().material;
+
+        gameObjectMat.color = simulationObject.Material.Color;
+        gameObjectMat.SetFloat("_Metallic", simulationObject.Material.Metallicness);
+        gameObjectMat.SetFloat("_Smoothness", simulationObject.Material.Smoothness);
+
+        gameObjectPhysicMat.bounciness = simulationObject.PhysicMaterial.bounciness;
+        gameObjectPhysicMat.dynamicFriction = simulationObject.PhysicMaterial.dynamicFriction;
+        gameObjectPhysicMat.staticFriction = simulationObject.PhysicMaterial.staticFriction;
+        gameObjectPhysicMat.frictionCombine = simulationObject.PhysicMaterial.frictionCombine;
+        gameObjectPhysicMat.bounceCombine = simulationObject.PhysicMaterial.bounceCombine;
     }
 
     void Update()
